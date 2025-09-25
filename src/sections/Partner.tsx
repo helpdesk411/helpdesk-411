b6eb6f143d8fa2391d467a5cd50be76a9b09c6b8
rnrnshn <olymangue@gmail.com>
2025-09-25 14:37:18 +0200

    feat: change partners image

--- src/sections/Partner.tsx
+++ src/sections/Partner.tsx
@@ -151,7 +151,7 @@
               
               {/* Image */}
               <div className="flex flex-col gap-32 md:mb-32">
-                <img src="/images/partners_image.svg" alt="Partner" className="hidden md:block w-full h-96 md:h-full md:object-cover object-center rounded-2xl" />
+                <img src="/images/partners_img.png" alt="Partner" className="hidden md:block w-3/4 mx-auto md:object-contain object-center rounded-2xl" />
 
                 <img src="/images/partners_image_mobile.svg" alt="Partner" className="block md:hidden w-full object-cover rounded-2xl" />
 
